eff50a599363252ab19406cff6467dd7d757b456
M.behzadfar <mo.behzad34@gmail.com>
2021-05-22 14:06:59 +0430

    change ImGuiCol_ModalWindowDarkening with ImGuiCol_ModalWindowDimBg

--- menu.cpp
+++ menu.cpp
@@ -54,7 +54,7 @@ namespace menu {
 		colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
 		colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
 		colors[ImGuiCol_TextSelectedBg] = ImVec4(0.26f, 0.59f, 0.98f, 0.35f);
-		colors[ImGuiCol_ModalWindowDarkening] = ImVec4(0.80f, 0.80f, 0.80f, 0.35f);
+		colors[ImGuiCol_ModalWindowDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.35f);
 		colors[ImGuiCol_DragDropTarget] = ImVec4(1.00f, 1.00f, 0.00f, 0.90f);
 
 		ImGuiWindowFlags	window_flags = 0;
